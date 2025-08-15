32cbf742d76e78894fd2f0255e6a80d6b9d9d0ff
Matheuszmt <matheusmpvphg@gmail.com>
2025-08-15 14:27:20 -0300

    Version 1.0

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import './App.css';
 import NavBar from './_components/NavBar';
-import Profile from './_components/Profile';
 import Card from './_components/Card';
 import Skills from './_components/Skills';
-
-
-const img = './my-portfolio/src/img/background.png'
-
 
 function App() {
   return (
